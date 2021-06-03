27595efe8bb09f594cdff44428a5422a5716bae7
valera <valera>
2021-06-04 00:08:13 +0300

    UI

---
+++ Costs/ .swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Costs
+//
+//  Created by Valera on 2.06.21.
+//  Copyright © 2021 Valera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var numberFromKeyboard: [UIButton]! {
+        didSet {
+            for button in numberFromKeyboard {
+                button.layer.cornerRadius = 11
+            }
+        }
+    }
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var displayLabel: UILabel!
+    
+    var stillTyping = false
+    var categoryName = ""
+    var displayValue = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        guard let number = sender.currentTitle else { return }
+        guard let displayText = displayLabel.text else { return }
+        if stillTyping {
+            if displayText.count < 15 {
+                displayLabel.text = displayText + number
+            }
+        } else {
+            displayLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func resetButtton(_ sender: UIButton) {
+        displayLabel.text = "0"
+        stillTyping = false
+    }
+    
+    @IBAction func categoryPressed(_ sender: UIButton) {
+        categoryName = sender.currentTitle!
+        displayValue = displayLabel.text!
+        displayLabel.text = "0"
+        stillTyping = false
+        print("\(categoryName), \(displayValue)")
+         
+    }
+}
+
+// MARK: TableViewDataSourse
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+         
+        return cell
+    }
+    
+    
+}
+
+// MARK: TableViewDelegate
+extension ViewController: UITableViewDelegate {
+    
+}
---
+++ Costs/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  Costs
+//
+//  Created by Valera on 3.06.21.
+//  Copyright © 2021 Valera. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var recordImage: UIImageView!
+    @IBOutlet var recordCategory: UILabel!
+    @IBOutlet var recordCost: UILabel!
+    
+}
